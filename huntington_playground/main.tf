70560f0c098e5e887c452ffaab9a75ad2d944509
chadm9 <chadm_@yahoo.com>
2022-06-28 08:31:54 -0400

    init commit

--- huntington_playground/main.tf
+++ huntington_playground/main.tf
@@ -1,4 +1,16 @@
-# VPC network
+
+#############################
+# Supporting Infrastructure #
+############################3
+
+/*
+This includes infrastructure that is required to support an internal http
+load balancer with a serverless Cloud Run NEG, but which is not a part of the ILB
+or NEG, and therefore may well be defined elsewhere.  Make sure the Supporting
+Infrastructure is in place before attempting to create the Core Infrastructure.
+*/
+
+# Creat a VPC network to house the internal load balancer (ilb)
 resource "google_compute_network" "ilb_network" {
   project                 = var.project_id
   name                    = "l7-ilb-network"
@@ -6,19 +18,7 @@ resource "google_compute_network" "ilb_network" {
   auto_create_subnetworks = false
 }
 
-# proxy-only subnet
-resource "google_compute_subnetwork" "proxy_subnet" {
-  name          = "l7-ilb-proxy-only-subnet"
-  provider      = google-beta
-  ip_cidr_range = "10.0.0.0/24"
-  region        = var.region
-  project       = var.project_id
-  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
-  role          = "ACTIVE"
-  network       = google_compute_network.ilb_network.id
-}
-
-# backend subnet
+# Create a subnet for the 'required' (see below) GCE instance
 resource "google_compute_subnetwork" "ilb_subnet" {
   name          = "l7-ilb-subnet"
   provider      = google-beta
@@ -28,183 +28,12 @@ resource "google_compute_subnetwork" "ilb_subnet" {
   network       = google_compute_network.ilb_network.id
 }
 
-# forwarding rule
-resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
-  project               = var.project_id
-  name                  = "l7-ilb-forwarding-rule"
-  provider              = google-beta
-  region                = "us-east1"
-  depends_on            = [google_compute_subnetwork.proxy_subnet]
-  ip_protocol           = "TCP"
-  load_balancing_scheme = "INTERNAL_MANAGED"
-  port_range            = "80"
-  target                = google_compute_region_target_http_proxy.default.id
-  network               = google_compute_network.ilb_network.id
-  subnetwork            = google_compute_subnetwork.ilb_subnet.id
-  network_tier          = "PREMIUM"
-}
-
-# HTTP target proxy
-resource "google_compute_region_target_http_proxy" "default" {
-  project  = var.project_id
-  name     = "l7-ilb-target-http-proxy"
-  provider = google-beta
-  region   = "us-east1"
-  url_map  = google_compute_region_url_map.default.id
-}
-
-# URL map
-resource "google_compute_region_url_map" "default" {
-  project         = var.project_id
-  name            = "l7-ilb-regional-url-map"
-  provider        = google-beta
-  region          = "us-east1"
-  default_service = google_compute_region_backend_service.default.id
-}
-
-# backend service
-resource "google_compute_region_backend_service" "default" {
-  project               = var.project_id
-  name                  = "l7-ilb-backend-service"
-  provider              = google-beta
-  region                = "us-east1"
-  protocol              = "HTTP"
-  load_balancing_scheme = "INTERNAL_MANAGED"
-  timeout_sec           = 30
-  backend {
-    group           = google_compute_region_network_endpoint_group.cloudrun_neg.id
-    balancing_mode  = ""
-  }
-}
-
-#resource "google_compute_backend_service" "default" {
-#  name      = "cloud-run-backend-service"
-#
-#  protocol  = "HTTP"
-#  port_name = "http"
-#  timeout_sec = 30
-#
-#  backend {
-#    group = google_compute_region_network_endpoint_group.cloudrun_neg.id
-#  }
-#}
-
-
-#resource "google_compute_region_backend_service" "default" {
-#  name                  = "l7-ilb-backend-subnet"
-#  provider              = google-beta
-#  region                = "us-east1"
-#  protocol              = "HTTP"
-#  load_balancing_scheme = "INTERNAL_MANAGED"
-#  timeout_sec           = 10
-#  health_checks         = [google_compute_region_health_check.default.id]
-#  backend {
-#    group           = google_compute_region_instance_group_manager.mig.instance_group
-#    balancing_mode  = "UTILIZATION"
-#    capacity_scaler = 1.0
-#  }
-#}
-
-# instance template
-#resource "google_compute_instance_template" "instance_template" {
-#  name         = "l7-ilb-mig-template"
-#  provider     = google-beta
-#  machine_type = "e2-small"
-#  tags         = ["http-server"]
-#
-#  network_interface {
-#    network    = google_compute_network.ilb_network.id
-#    subnetwork = google_compute_subnetwork.ilb_subnet.id
-#    access_config {
-#      # add external ip to fetch packages
-#    }
-#  }
-#  disk {
-#    source_image = "debian-cloud/debian-10"
-#    auto_delete  = true
-#    boot         = true
-#  }
-#
-#  # install nginx and serve a simple web page
-#  metadata = {
-#    startup-script = <<-EOF1
-#      #! /bin/bash
-#      set -euo pipefail
-#
-#      export DEBIAN_FRONTEND=noninteractive
-#      apt-get update
-#      apt-get install -y nginx-light jq
-#
-#      NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
-#      IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
-#      METADATA=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=True" | jq 'del(.["startup-script"])')
-#
-#      cat <<EOF > /var/www/html/index.html
-#      <pre>
-#      Name: $NAME
-#      IP: $IP
-#      Metadata: $METADATA
-#      </pre>
-#      EOF
-#    EOF1
-#  }
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#}
-
-# health check
-#resource "google_compute_region_health_check" "default" {
-#  name     = "l7-ilb-hc"
-#  provider = google-beta
-#  region   = "us-east1"
-#  http_health_check {
-#    port_specification = "USE_SERVING_PORT"
-#  }
-#}
-
-# MIG
-#resource "google_compute_region_instance_group_manager" "mig" {
-#  name     = "l7-ilb-mig1"
-#  provider = google-beta
-#  region   = "us-east1"
-#  version {
-#    instance_template = google_compute_instance_template.instance_template.id
-#    name              = "primary"
-#  }
-#  base_instance_name = "vm"
-#  target_size        = 2
-#}
-
-# allow all access from IAP and health check ranges
-resource "google_compute_firewall" "fw-iap" {
-  project       = var.project_id
-  name          = "l7-ilb-fw-allow-iap-hc"
-  provider      = google-beta
-  direction     = "INGRESS"
-  network       = google_compute_network.ilb_network.id
-  source_ranges = ["130.211.0.0/22", "35.191.0.0/16", "35.235.240.0/20"]
-  allow {
-    protocol = "tcp"
-  }
-}
-
-# allow http from proxy subnet to backends
-resource "google_compute_firewall" "fw-ilb-to-backends" {
-  project       = var.project_id
-  name          = "l7-ilb-fw-allow-ilb-to-backends"
-  provider      = google-beta
-  direction     = "INGRESS"
-  network       = google_compute_network.ilb_network.id
-  source_ranges = ["10.0.0.0/24"]
-  target_tags   = ["http-server"]
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443", "8080"]
-  }
-}
-
-# test instance
+/*
+According to GCP:
+"There must be at least one VM in the VPC network in which you intend
+to set up a regional load balancer with a serverless backend."
+https://cloud.google.com/load-balancing/docs/l7-internal/setting-up-l7-internal-serverless#gcloud_1
+*/
 resource "google_compute_instance" "vm-test" {
   project      = var.project_id
   name         = "l7-ilb-test-vm"
@@ -222,26 +51,25 @@ resource "google_compute_instance" "vm-test" {
   }
 }
 
-//---------------------------------------------
-# CLOUD RUN
-//---------------------------------------------
-
-resource "google_compute_region_network_endpoint_group" "cloudrun_neg" {
-  project               = var.project_id
-  provider              = google-beta
-  name                  = "cloud-run-neg"
-  network_endpoint_type = "SERVERLESS"
-  region                = var.region
-  cloud_run {
-    service = google_cloud_run_service.default.name
+/*
+--OPTIONAL--
+Create a firewall rule to whitelist access from IAP so the required GCE instance (see
+google_compute_instance.vm-test), which has no external ip, is accessible via ssh
+(assuming the user trying to ssh has the proper roles assigned, e.g., editor).
+*/
+resource "google_compute_firewall" "fw-iap" {
+  project       = var.project_id
+  name          = "l7-ilb-fw-allow-iap-hc"
+  provider      = google-beta
+  direction     = "INGRESS"
+  network       = google_compute_network.ilb_network.id
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16", "35.235.240.0/20"]
+  allow {
+    protocol = "tcp"
   }
 }
 
-
-//---------------------------------------------
-# CLOUD RUN
-//---------------------------------------------
-
+# Create a Cloud Run service
 resource "google_cloud_run_service" "default" {
   name     = "hello-service"
   location = var.region
@@ -256,10 +84,104 @@ resource "google_cloud_run_service" "default" {
   }
 }
 
+/*
+Allow anyone (i.e. allUsers) to invoke a Cloud Run container by issuing a
+GET request to its HTTP(s) endpoint
+*/
 resource "google_cloud_run_service_iam_member" "member" {
   location = google_cloud_run_service.default.location
   project  = google_cloud_run_service.default.project
   service  = google_cloud_run_service.default.name
   role     = "roles/run.invoker"
   member   = "allUsers"
+}
+
+
+
+#############################
+#    Core Infrastructure    #
+#############################
+
+/*
+Infrastructure which directly defines the internal HTTP ILB and its serverless
+Cloud Run NEG.  If creating ensure the Supporing Infrastructure is created and
+properly referenced.
+*/
+
+/*
+Create the required proxy only subnet for load balancer
+source ip addresses as seen by backends. Only one is needed per
+region and is capable of supporting multiple envoy based lbs.
+https://cloud.google.com/load-balancing/docs/proxy-only-subnets
+*/
+resource "google_compute_subnetwork" "proxy_subnet" {
+  name          = "l7-ilb-proxy-only-subnet"
+  provider      = google-beta
+  ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
+  project       = var.project_id
+  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
+  role          = "ACTIVE"
+  network       = google_compute_network.ilb_network.id
+}
+
+# Create the ilb forwarding rule
+resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
+  project               = var.project_id
+  name                  = "l7-ilb-forwarding-rule"
+  provider              = google-beta
+  region                = "us-east1"
+  depends_on            = [google_compute_subnetwork.proxy_subnet]
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "INTERNAL_MANAGED"
+  port_range            = "80"
+  target                = google_compute_region_target_http_proxy.default.id
+  network               = google_compute_network.ilb_network.id
+  subnetwork            = google_compute_subnetwork.ilb_subnet.id
+  network_tier          = "PREMIUM"
+}
+
+# Create the ilb HTTP target proxy
+resource "google_compute_region_target_http_proxy" "default" {
+  project  = var.project_id
+  name     = "l7-ilb-target-http-proxy"
+  provider = google-beta
+  region   = "us-east1"
+  url_map  = google_compute_region_url_map.default.id
+}
+
+# Create the ilb URL map
+resource "google_compute_region_url_map" "default" {
+  project         = var.project_id
+  name            = "l7-ilb-regional-url-map"
+  provider        = google-beta
+  region          = "us-east1"
+  default_service = google_compute_region_backend_service.default.id
+}
+
+# Create the backend service
+resource "google_compute_region_backend_service" "default" {
+  project               = var.project_id
+  name                  = "l7-ilb-backend-service"
+  provider              = google-beta
+  region                = "us-east1"
+  protocol              = "HTTP"
+  load_balancing_scheme = "INTERNAL_MANAGED"
+  timeout_sec           = 30
+  backend {
+    group           = google_compute_region_network_endpoint_group.cloudrun_neg.id
+    balancing_mode  = ""
+  }
+}
+
+# Create the Cloud Run Network Endpoint Group (NEG).
+resource "google_compute_region_network_endpoint_group" "cloudrun_neg" {
+  project               = var.project_id
+  provider              = google-beta
+  name                  = "cloud-run-neg"
+  network_endpoint_type = "SERVERLESS"
+  region                = var.region
+  cloud_run {
+    service = google_cloud_run_service.default.name
+  }
 }
